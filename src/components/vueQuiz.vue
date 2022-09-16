d5c3a22d8f4dbc5a4c1e6b0b136f9e009a3b84b2
Aleksandr <for.melnik.dev@gmail.com>
2022-09-16 16:14:41 +0300

    component

---
+++ src/components/vueQuiz.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="progress">
+    <div :style="{'width': '30%'}"  class="progress__inner"></div>
+  </div>
+  <h1>{{ question }}</h1>
+  <ul>
+    <li>Это функция для хранения данных компонента</li>
+    <li>Это глобальный стейт</li>
+    <li>Это когда на ты никому не нужен</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "vueQuiz",
+  props: ["question"]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/vueResult.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="result">
+    <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+    <h2>Вы отгадали 3 ответа из 10</h2>
+    <button>Попробовать снова</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vueResult"
+}
+</script>
+
+<style scoped>
+
+</style>
